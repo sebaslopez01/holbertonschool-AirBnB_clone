d0febc3727603d3194de8116ae1fd28a555dc1a8
Sebastian <sebaslopez.2002@gmail.com>
2022-10-24 23:55:08 -0500

    Tests definitions

---
+++ tests/test_models.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+
+This module define test classes
+
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
+
+class TestFileStorage(unittest.TestCase):
+    pass
+
+
+class TestBaseModel(unittest.TestCase):
+    pass
+
+
+class TestUser(unittest.TestCase):
+    pass
+
+
+class TestPlace(unittest.TestCase):
+    pass
+
+
+class TestState(unittest.TestCase):
+    pass
+
+
+class TestCity(unittest.TestCase):
+    pass
+
+
+class TestReview(unittest.TestCase):
+    pass
+
+
+class TestAmenity(unittest.TestCase):
+    pass

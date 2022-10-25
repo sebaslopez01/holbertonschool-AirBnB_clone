4562e72578cc7b3124eeae4e3cae8e9d0440c01c
Sebastian <sebaslopez.2002@gmail.com>
2022-10-25 09:47:51 -0500

    Bug fixed in tests user model

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,25 +16,21 @@ class TestUser(unittest.TestCase):
     """
 
     def test_email_attribute(self):
-        user = User()
-        user.email = 'example@gmail.com'
+        User.email = 'example@gmail.com'
 
-        self.assertEqual(user.email, 'example@gmail.com')
+        self.assertEqual(User.email, 'example@gmail.com')
 
     def test_password_attribute(self):
-        user = User()
-        user.password = 'password'
+        User.password = 'password'
 
-        self.assertEqual(user.password, 'password')
+        self.assertEqual(User.password, 'password')
 
     def test_first_name_attribute(self):
-        user = User()
-        user.first_name = 'Juan'
+        User.first_name = 'Juan'
 
-        self.assertEqual(user.first_name, 'Juan')
+        self.assertEqual(User.first_name, 'Juan')
 
     def test_last_name_attribute(self):
-        user = User()
-        user.last_name = 'López'
+        User.last_name = 'López'
 
-        self.assertEqual(user.last_name, 'López')
+        self.assertEqual(User.last_name, 'López')

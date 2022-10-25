25ed6aba97dcdd1d5259e383e21266a272eda943
Sebastian <sebaslopez.2002@gmail.com>
2022-10-24 20:27:42 -0500

    to_dict method bug fixed

--- models/base_model.py
+++ models/base_model.py
@@ -35,10 +35,11 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['created_at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'

424eb415dd12f85111487e33428f058b9eb23bfa
Sebastian <sebaslopez.2002@gmail.com>
2022-10-25 09:00:35 -0500

    all tests for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,7 @@ This module define a TestBaseModel Class
 
 import unittest
 import os
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -22,10 +23,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(os.path.exists('file.json'))
         os.remove('file.json')
 
+    def test_to_dict_method(self):
+        bm = BaseModel()
+        self.assertTrue(bm.to_dict())
+
     def test_id_attribute(self):
         bm = BaseModel()
         self.assertTrue(bm.id)
 
+    def test_created_at_attribute(self):
+        self.assertEqual(BaseModel().created_at, datetime.now())
+
     def test_sting_representation(self):
         bm = BaseModel()
-        self.assertEqual(str(bm), f'[BaseModel] ({bm.id} {bm.__dict__})')
+        self.assertEqual(str(bm), f'[BaseModel] ({bm.id}) {bm.__dict__}')

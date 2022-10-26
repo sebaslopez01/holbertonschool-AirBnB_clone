2f659437ffaa009394e86ef4a21d8a5eb7bd0612
Sebastian <sebaslopez.2002@gmail.com>
2022-10-26 09:29:18 -0500

    Tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -7,7 +7,29 @@ This module define a TestAmenity Class
 """
 
 import unittest
+import os
+from models.base_model import BaseModel
 
 
 class TestGeneral(unittest.TestCase):
-    pass
+    def test_save_method(self):
+        bm = BaseModel()
+        bm.save()
+        self.assertTrue(os.path.exists('file.json'))
+        os.remove('file.json')
+
+    def test_to_dict_method(self):
+        bm = BaseModel()
+        self.assertTrue(bm.to_dict())
+
+    def test_id_attribute(self):
+        bm = BaseModel()
+        self.assertTrue(bm.id)
+
+    def test_created_at_attribute(self):
+        bm = BaseModel()
+        self.assertTrue(bm.created_at)
+
+    def test_sting_representation(self):
+        bm = BaseModel()
+        self.assertEqual(str(bm), f'[BaseModel] ({bm.id}) {bm.__dict__}')
